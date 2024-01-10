f3e17a64b46ce26174ad38a2ca0d0ca3c6f19f81
Matthew Haywood <43278160+mbh1620@users.noreply.github.com>
2024-01-10 10:26:38 +0000

    Development (#3)
    * added projectionViewer, wireframe, edge, camera and face classes
    
    * added controls and move camera functions to the projection viewer function
    
    * added a function for removing negative projection
    
    * added functions to clip faces against a plane
    
    * added abs to projection to stop inverting problem from happening
    
    * worked out how to do clipping in the z axis
    
    * made a start implementing the readwriter class
    
    * added clipping for nodes
    
    * sorted problem out with winding order of the generated triangle when the d2 point is the clipped point or the point which is left
    
    * added a material class
    
    * added edge clipping
    
    * implementing the shading function
    
    * refactored the displayFaces function
    
    * implemented shading with a single light source
    
    * changed code
    
    * added the capability of allowing multiple coloured lights to be added to the renderer
    
    * Cleaned the project files
    
    * cleaned up the working branch
    
    * added blender cube model

--- main.py
+++ main.py
@@ -1,13 +1,5 @@
 from Classes.ProjectionViewer import ProjectionViewer
 from Classes.Wireframe import Wireframe
-from Classes.Edge import Edge
-from Classes.Face import Face
-from Classes.Light import Light
-from Classes.FileWriteReader import FileWriteReader
-from Functions.cubeGenerator import cubeGenerator
-from Functions.axisGenerator import axisGenerator
-from Functions.planeGenerator import planeGenerator
-from Functions.normaliseVector import crossProduct
 import numpy as np
 
 centerPoint = Wireframe()
@@ -18,14 +10,7 @@ projectionViewer1 = ProjectionViewer(1200,1000, centerPoint)
 	
 projectionViewer1.addWireframe('centerPoint', centerPoint)
 
-# projectionViewer1.openFile('./blenderCube.obj', 1000)
-
-# projectionViewer1.wireframes['./blenderCube.obj2'].displayNodes = False
-
-projectionViewer1.openFile('./blenderCube.obj', 500)
-
-# plane1 = planeGenerator('X', 0, 1000)
-
-# projectionViewer1.addWireframe('plane1', plane1)
+projectionViewer1.openFile('./blenderCube.obj', 1000)
+projectionViewer1.wireframes['./blenderCube.obj2'].displayNodes = False
 
 projectionViewer1.run()
